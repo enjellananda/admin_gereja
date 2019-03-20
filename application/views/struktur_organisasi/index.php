98205f0ad59036729815b0500f5c766b90e9323b
ENJELLA\WINDOWS 8.1 <enjellananda@github.com>
2019-03-20 09:28:22 +0700

    tambahan table

--- application/views/struktur_organisasi/index.php
+++ application/views/struktur_organisasi/index.php
@@ -3,7 +3,7 @@
     <div class="col-md-12">
         <div class="box">
             <div class="box-header">
-                <h3 class="box-title">Jemaat</h3>
+                <h3 class="box-title">Struktur Organisasi</h3>
                 <div class="box-tools">
                     <a href="<?php echo site_url('jemaat/add'); ?>" class="btn btn-success btn-sm">Add</a>
                 </div>
